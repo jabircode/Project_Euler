f11f068d9711636d9ab9816599c57dbca253ce3a
jabircode <105219021+jabircode@users.noreply.github.com>
2022-12-29 10:16:49 +0800

    Update Problem1_Multiples_of_3_and_5.js

--- Problem1_Multiples_of_3_and_5.js
+++ Problem1_Multiples_of_3_and_5.js
@@ -2,14 +2,20 @@
 // Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
 
 function multiplesOf3and5(number) {
-  
-let sum = 0;
+  // Initialize a variable to store the sum of the multiples
+  let sum = 0;
+
+  // Iterate over all the numbers from 3 to 'number' - 1
   for (let i = 3; i < number; i++) {
+    // If 'i' is a multiple of 3 or 5, add it to the sum
     if (i % 3 == 0 || i % 5 == 0) {
       sum += i;
     }
   }
+
+  // Return the sum of the multiples
   return sum;
 }
 
-multiplesOf3and5(1000);
+// Find the sum of the multiples of 3 and 5 below 1000
+console.log(multiplesOf3and5(1000)); // 233168
